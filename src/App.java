979738bcce74c9f80254c0a38c483e77131f91ac
Eri2005 <eri.costa05@gmail.com>
2023-03-28 00:52:12 -0300

    Extrair só os dados que interessam (titulo, poster, classificação)

--- src/App.java
+++ src/App.java
@@ -3,22 +3,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
-        
         URI endereco = URI.create(url);
-        
         var client = HttpClient.newHttpClient();
-
         var request = HttpRequest.newBuilder(endereco).GET().build();
-
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-
         String body = response.body();
-
         System.out.println(body);
+
+        // Extrair só os dados que interessam (titulo, poster, classificação)
+        // Vai ser usado uma lista em vez de um Array
+        //Map é um dicionario que associa uma chave com um valor
+        // Agora dentro de Map informar que tipo. Que é txto 'String'
+        List<Map<>>
     }
 }
