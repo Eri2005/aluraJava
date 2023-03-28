46a4c56624f17b12c793b0032dcb24cf0b042a02
Eri2005 <eri.costa05@gmail.com>
2023-03-28 00:36:06 -0300

    Removendo os comentarios da Instrução anterior

--- src/App.java
+++ src/App.java
@@ -7,24 +7,16 @@ import java.net.http.HttpResponse.BodyHandlers;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // Fazer uma conexão HTTP e buscar Top 250 filmes
-        // Jogando URL da API em uma variavel
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         
-        // Criando uma URI pegando a variavel URL
         URI endereco = URI.create(url);
         
-        // Criando um HttpClient com newHttpClient que é metodo estatico
-        // Jogando em uma variavel 'client'
         var client = HttpClient.newHttpClient();
 
-        // Pegando o conteudo da requisição
         var request = HttpRequest.newBuilder(endereco).GET().build();
 
-        // Pegando o resultado
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
-        // Pegando o body da requisição
         String body = response.body();
 
         System.out.println(body);
